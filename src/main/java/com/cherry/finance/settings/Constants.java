664b5f4fd41930445ffb44ae37f2890de6b2e679
Vikram <vikram.anthamgari@gmail.com>
2023-08-09 12:17:46 +0530

    Added token for auth

--- src/main/java/com/cherry/finance/settings/Constants.java
+++ src/main/java/com/cherry/finance/settings/Constants.java
@@ -3,4 +3,5 @@ package com.cherry.finance.settings;
 public class Constants {
     public static final int NUM_OF_DAYS = 100;
     public static final float INTEREST_RATE = 0.15f;
+    public static final String TOKEN = "(fLyEkA3q$Q^QbZ@QCWt94pv2XxbWcFx";
 }
